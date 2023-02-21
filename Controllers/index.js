3d315050e1c96c166483a89c27f426a63792732e
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-02-21 13:46:22 -0700

    Added seeds and tested routes

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,63 +1,82 @@
-const Review = require("../models/reviews");
-const Rcoaster = require("../models/rollercoasters");
+const Review = require('../models/reviews')
+const Rcoaster = require('../models/rollercoasters')
 
 const createReview = async (req, res) => {
   try {
-    const review = await new Review(req.body);
-    await review.save();
+    const review = await new Review(req.body)
+    await review.save()
     return res.status(201).json({
-      review,
-    });
+      review
+    })
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+    return res.status(500).json({ error: error.message })
   }
-};
+}
 
 const getAllReviews = async (req, res) => {
   try {
-    const reviews = await Review.find();
-    return res.status(200).json({ reviews });
+    const reviews = await Review.find()
+    return res.status(200).json({ reviews })
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const getReviewById = async (req, res) => {
   try {
-    const { id } = req.params;
-    const review = await Review.findById(id);
+    const { id } = req.params
+    const review = await Review.findById(id)
     if (review) {
-      return res.status(200).json({ plant });
+      return res.status(200).json({ plant })
     }
-    return res.status(404).send("Review with the specified ID does not exists");
+    return res.status(404).send('Review with the specified ID does not exists')
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const deleteReview = async (req, res) => {
   try {
-    const { id } = req.params;
-    const deleted = await Review.findByIdAndDelete(id);
+    const { id } = req.params
+    const deleted = await Review.findByIdAndDelete(id)
     if (deleted) {
-      return res.status(200).send("Review deleted");
+      return res.status(200).send('Review deleted')
     }
-    throw new Error("Review not found");
+    throw new Error('Review not found')
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
 
 const updateReview = async (req, res) => {
   try {
     const review = await Review.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-    });
-    res.status(200).json(review);
+      new: true
+    })
+    res.status(200).json(review)
   } catch (error) {
-    return res.status(500).send(error.message);
+    return res.status(500).send(error.message)
   }
-};
+}
+const getAllRides = async (req, res) => {
+  try {
+    const rides = await Rcoaster.find()
+    return res.status(200).json({ rides })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+const createRides = async (req, res) => {
+  try {
+    const rides = await new Rcoaster(req.body)
+    await rides.save()
+    return res.status(201).json({
+      rides
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
 
 module.exports = {
   createReview,
@@ -65,4 +84,6 @@ module.exports = {
   getReviewById,
   deleteReview,
   updateReview,
-};
+  getAllRides,
+  createRides
+}
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,9 @@
 const { Router } = require('express')
-const controllers = require('../Controllers')
+const controllers = require('../Controllers/')
 const router = Router()
+
+router.get('/', (req, res) => res.send('DB connected!'))
+
+router.post('/rides', controllers.createRides)
+
+module.exports = router
