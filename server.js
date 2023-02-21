9879b213b9414890c22fe4008f89aae097abd43e
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-02-21 11:54:38 -0700

    Added to server JS and started models

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const routes = require('./routes')
+const db = require('./db/index')
+const logger = require('morgan')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(logger('dev'))
+app.use('/api', routes)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
