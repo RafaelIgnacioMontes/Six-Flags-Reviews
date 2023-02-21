39b88e295abc44a9816c9909b0f0834203b35fdd
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-02-21 12:38:21 -0700

    Working on seeds pt 2

--- Seeds/rides.js
+++ Seeds/rides.js
@@ -8,9 +8,17 @@ const main = async () => {
     {
       name: 'Jersey Devil Coaster',
       picture: 'https://rcdb.com/aafxzca',
-      year: '2021',
-      height: '48"',
-      time: ''
+      year_built: '2021',
+      height_requirement: '48"',
+      ride_time: '2:05',
+      max_speed: '58mph',
+      intensity: 'Maximum'
+    },
+    {
+      name: 'Green Lantern',
+      picture:
+        'https://static.wikia.nocookie.net/sixflags/images/6/67/GreenLanternSFGADV.jpg/revision/latest?cb=20180629095710',
+      year_built: ''
     }
   ]
 }
