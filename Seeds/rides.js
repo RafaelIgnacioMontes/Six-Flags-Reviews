f7959bcae7b194f076162d884060d86a55f3e3a4
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-02-21 12:13:09 -0700

    forgot to save

--- Seeds/rides.js
+++ Seeds/rides.js
@@ -1,2 +1,16 @@
 const db = require('../db')
 const rollercoasters = require('../models/rollercoasters')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const main = async () => {
+  const rcoasters = [
+    {
+      name: 'Jersey Devil Coaster',
+      picture: 'https://rcdb.com/aafxzca',
+      year: '2021',
+      height: '48"',
+      time: ''
+    }
+  ]
+}
