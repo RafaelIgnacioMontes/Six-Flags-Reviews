19444266f6bea92a572e82c2150a3acb1fb36049
RafaelIgnacioMontes <rafaellending@gmail.com>
2023-02-22 08:00:53 -0700

    Worked on get all for rides

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,29 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const Home = () => {
-  return <></>
+  const [rideList, setRideList] = useState([])
+
+  const getAllRides = async () => {
+    const response = await axios.get('http://localhost:3001/api/rides')
+    console.log(response.data.rides)
+    setRideList(response.data.rides)
+  }
+  useEffect(() => {
+    getAllRides()
+  }, [])
+  return (
+    <div>
+      <h1></h1>
+      <div>
+        {rideList.map((rides) => {
+          ;<div>
+            <div>{rides.name}</div>
+          </div>
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Home
